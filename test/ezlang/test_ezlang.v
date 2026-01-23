8662582e7d2fb82ce30f9b8543ba58f74ce4e851
cryingn <cryingnights7v@qq.com>
2026-01-24 02:18:27 +0800

    ci_linux: test

--- test/ezlang/test_ezlang.v
+++ test/ezlang/test_ezlang.v
@@ -6,7 +6,8 @@ import os
 pub fn test_ezlang() {
 	files := tools.walk_files(ezlang_test_path)
 	for file in files {
-		result := os.execute("dotnet run --project ../make --no-build json ${file}")
+		//result := os.execute("dotnet run --project ../make --no-build json ${file}")
+		result := os.execute("dotnet run --project ../make json ${file}")
 		load_data := result.output.trim_space()
 		read_data := tools.read_file(file).split("@[exit]")[1].trim_space()
 		if load_data != read_data {
